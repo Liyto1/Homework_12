bf8a08d75db2b93fb924baa0da1b2fc05096aec3
Lev <lev.lvi2@gmail.com>
2023-07-07 13:09:32 +0300

    done

--- src/SecondTask/FizzBuzzClass.java
+++ src/SecondTask/FizzBuzzClass.java
@@ -6,22 +6,25 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class FizzBuzzClass {
     private static final int number = 15;
     private static final BlockingQueue<String> queue = new LinkedBlockingQueue<>();
-    private static Object lock = new Object();
+    private static final Object lock = new Object();
+    private static int counter = 1;
 
     public static void main(String[] args) {
 
         Thread aThread = new Thread(() -> {
-            for (int i = 1; i <= number; i++) {
+            while (counter <= number) {
                 synchronized (lock) {
-                    if (i % 3 == 0) {
+                    if (counter % 3 == 0 && counter % 5 != 0) {
                         try {
                             queue.put("fizz");
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
+                        counter++;
+                        lock.notifyAll();
                     } else {
                         try {
-                            queue.put(Integer.toString(i));
+                            lock.wait();
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
@@ -31,12 +34,19 @@ public class FizzBuzzClass {
         });
 
         Thread bThread = new Thread(() -> {
-            for (int i = 1; i <= number; i++) {
+            while (counter <= number) {
                 synchronized (lock) {
-                    if (i % 5 == 0) {
-
+                    if (counter % 5 == 0 && counter % 3 != 0) {
                         try {
                             queue.put("buzz");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        counter++;
+                        lock.notifyAll();
+                    } else {
+                        try {
+                            lock.wait();
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
@@ -46,12 +56,19 @@ public class FizzBuzzClass {
         });
 
         Thread cThread = new Thread(() -> {
-            for (int i = 1; i <= number; i++) {
+            while (counter <= number) {
                 synchronized (lock) {
-                    if (i % 3 == 0 && i % 5 == 0) {
-
+                    if (counter % 3 == 0 && counter % 5 == 0) {
                         try {
                             queue.put("fizzbuzz");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        counter++;
+                        lock.notifyAll();
+                    } else {
+                        try {
+                            lock.wait();
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
@@ -61,11 +78,23 @@ public class FizzBuzzClass {
         });
 
         Thread dThread = new Thread(() -> {
-            for (int i = 1; i <= number; i++) {
-                try {
-                    System.out.println(queue.take());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            while (counter <= number) {
+                synchronized (lock) {
+                    if (counter % 3 != 0 && counter % 5 != 0) {
+                        try {
+                            queue.put(Integer.toString(counter));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        counter++;
+                        lock.notifyAll();
+                    } else {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
             }
         });
@@ -74,5 +103,22 @@ public class FizzBuzzClass {
         bThread.start();
         cThread.start();
         dThread.start();
+
+        try {
+            aThread.join();
+            bThread.join();
+            cThread.join();
+            dThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        while (!queue.isEmpty()) {
+            try {
+                System.out.println(queue.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
